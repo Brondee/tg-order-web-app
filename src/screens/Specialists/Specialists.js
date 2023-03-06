edb803c355a115fe1ae0db26e7f799db4e64943e
Artyom <3Westers07P@gmail.com>
2023-03-06 23:19:39 +0300

    changed styles again

--- src/screens/Specialists/Specialists.js
+++ src/screens/Specialists/Specialists.js
@@ -9,18 +9,20 @@ import { specialists } from "../../data";
 const Specialists = () => {
   return (
     <div class="main-container">
-      <h1 class="main-title">Выберите специалиста</h1>
-      <div className="components-container">
-        {specialists.map((person) => {
-          const { name, qualification, photo } = person;
-          return (
-            <Specialist
-              name={name}
-              qualification={qualification}
-              photo={photo}
-            />
-          );
-        })}
+      <div className="wrap">
+        <h1 class="main-title">Выберите специалиста</h1>
+        <div className="components-container">
+          {specialists.map((person) => {
+            const { name, qualification, photo } = person;
+            return (
+              <Specialist
+                name={name}
+                qualification={qualification}
+                photo={photo}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
