9167e12e5c4f1fcd12c39a30430ed3552aeeeeb2
Artyom <3Westers07P@gmail.com>
2023-03-06 23:12:21 +0300

    changed styles

--- src/components/Specialist/Specialist.jsx
+++ src/components/Specialist/Specialist.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./Specialist.css";
+import plus from "../../assets/img/plus.png";
 
 const Specialist = ({ name, qualification, photo }) => {
   return (
     <div class="container">
-      <img src={photo} alt="person img" class="photo" />
-      <div>
-        <h3 className="name">{name}</h3>
-        <p className="qualification">{qualification}</p>
+      <div className="img-name-container">
+        <img src={photo} alt="person img" class="photo" />
+        <div class="name-qual-container">
+          <h3 className="name">{name}</h3>
+          <p className="qualification">{qualification}</p>
+        </div>
       </div>
       <div class="plus-circle">
-        <img src="" alt="plus" class="plus" />
+        <img src={plus} alt="plus" class="plus" />
       </div>
     </div>
   );
